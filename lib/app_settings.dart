bb937c46ca8037c125237e75bc2ab3d39d4f66e8
fatao0330-commits <fatao0330@gmail.com>
2026-09-10 18:54:16 +0000

    modifier 2 croire

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -4,7 +4,6 @@ class AppSettings {
   AppSettings._();
 
   static const _localeKey = 'app_locale';
-  static const _pinKey = 'settings_pin';
 
   static Future<String?> loadLocale() async {
     final preferences = await SharedPreferences.getInstance();
@@ -14,18 +13,5 @@ class AppSettings {
   static Future<void> saveLocale(String languageCode) async {
     final preferences = await SharedPreferences.getInstance();
     await preferences.setString(_localeKey, languageCode);
-  }
-
-  static Future<String?> loadPin() async {
-    final preferences = await SharedPreferences.getInstance();
-    return preferences.getString(_pinKey);
-  }
-
-  static Future<void> savePin(String pin) async {
-    final preferences = await SharedPreferences.getInstance();
-    final saved = await preferences.setString(_pinKey, pin);
-    if (!saved) {
-      throw StateError('Le code PIN n’a pas pu être enregistré.');
-    }
   }
 }
